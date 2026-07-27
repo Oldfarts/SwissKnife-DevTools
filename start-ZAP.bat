f0546c035b7053a47fff5e36b8977e4399f28c69
Jani Ärväs <109293527+Oldfarts@users.noreply.github.com>
2026-07-27 16:02:49 +0300

    Fixed start-ZAP.bat
    1. Fixed opening the ZAP.bat -> opens only 1 cmd.exe
    2. Added mini-api.json to be able to run as part of the vite:
    http://localhost:5173/mini-api.json

--- start-ZAP.bat
+++ start-ZAP.bat
@@ -1,13 +1,12 @@
 @echo off
 setlocal
 
-echo 🚀 Käynnistetään ZAP daemon omassa ikkunassaan...
+echo 🚀 Käynnistetään ZAP daemon tässä ikkunassa...
 set ZAP_JAVA_OPTIONS=-Xmx6G -Xms1G
 
 cd /d "C:\Program Files\ZAP\Zed Attack Proxy"
 
-:: Avataan TÄYSIN ERILLINEN komentorivi-ikkuna ZAP:lle
-start "ZAP Daemon Window" cmd /k "Zap.bat -daemon -port 8080 -config api.disablekey=true"
+:: Käynnistetään suoraan ilman "start"-komentoa (ei avaa uutta ikkunaa)
+call Zap.bat -daemon -port 8080 -config api.disablekey=true
 
-echo ✅ ZAP-ikkuna avattu taustalle!
 endlocal
